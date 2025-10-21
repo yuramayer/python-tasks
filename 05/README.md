468dbd37dcf3960307e376e600bfdc3c028ae86d
yuramayer <votetopovorot2015@gmail.com>
2025-10-21 17:42:13 +0300

    feat: added another task with freq

---
+++ 05/README.md
@@ -0,0 +1,3 @@
+# Top K elems
+
+Given an integer array nums and an integer k, return the k most frequent elements in any order, O(n)
---
+++ 05/sol.py
@@ -0,0 +1,30 @@
+def top_freq(nums, k):
+
+    freq = {}
+
+    for el in nums:
+        freq[el] = freq.get(el, 0) + 1
+    
+    opfreq = {}
+
+    for key, value in freq.items():
+        if key in opfreq:
+            opfreq[value] = opfreq[value] + [key]
+        else:
+            opfreq[value] = [key]
+    
+    search_freq = len(nums)
+    res = []
+
+    while k > 0:
+        if search_freq < 1:
+            break
+        if search_freq in opfreq:
+            res.extend(opfreq[search_freq])
+            k -= 1
+        search_freq -= 1
+
+    return res
+
+
+print(top_freq([1, 1, 1, 1, 1, 3, 2, 2, 1, 5, 2, 2, 4, 1, 1, 2, 4], 3))
